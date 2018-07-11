7f6e54e7ed87595a344020af2f977dca653d315b
George Colon <geocolon@gmail.com>
2018-07-10 20:31:24 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,71 @@
+'use strict';
+/* global socket, sendStatus */
+
+const MongoClient = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+// console.log('This is MongoClient in action...: ',MongoClient);
+// Connect to mongo
+MongoClient.connect('mongodb://localhost:27017/mongochat',{useNewUrlParser: true }, function(err, db) {
+  
+  if(err){
+    throw err;
+  }
+  
+
+  // Connect to Socket.io
+  console.log('MongoDB ONLINE...');
+  client.on('connection', function(socket){
+    let chat = db.collection('chats');
+
+    // Create function to send status
+    let sendStatus = function(s){
+      socket.emit('status', s);
+    };
+
+    chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+      if(err) {
+        throw err;
+      }
+
+      // emit the messages
+      socket.emit('output', res);
+
+      // Handle input events
+      socket.on('input', function(date){
+        let name = data.name;
+        let message = data.message;
+
+        // Check for name and message
+        if(name === '' || message === '') {
+        // Send error status
+          sendStatus('Please enter a name and message');
+        } else {
+          // Insert message
+          chat.insert({name : name, message: message}, function() {
+            client.emit('output', [data]);
+
+            // Send status object
+            sendStatus({
+              message: 'Message sent',
+              clear: true
+            });
+
+          });
+        }
+      });
+
+      // Handle clear
+      socket.on('clear', function(data){
+        // Remove all chats from collection
+        chat.remove({}, function(){
+        // Emit cleared
+          socket.emit('cleared');
+        
+        });
+        
+      });
+    });
+  });
+
+
+});
